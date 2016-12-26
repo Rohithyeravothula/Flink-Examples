02f14c21bd2ee67e2786fc8d8f8344ceaa365acd
Rohith <rohith@sigmoidanalytics.com>
2016-12-26 13:54:41 +0530

    adds dependency files and word count example

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Flink-Test"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.flink" % "flink-scala_2.10" % "1.0.3"
+
+libraryDependencies += "org.apache.flink" % "flink-clients_2.10" % "1.0.3"
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,16 @@
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+/**
+  * Created by rohith on 26/12/16.
+  */
+
+object WordCount {
+  def main(args: Array[String]) = {
+    val eEnv =  ExecutionEnvironment.getExecutionEnvironment
+    val text = eEnv.fromElements("hello flink this is a test to sample sentence to see how does wordcount example work on flink. Things seem" +
+      "pretty awesome so far. Lets also see how does the streaming on flink work after this exaple")
+    val counts = text.flatMap{ sent => sent.toLowerCase().split("\\W+")}.map((_, 1)).groupBy(0).sum(1)
+    counts.print()
+  }
+}
